31accf2cfb219fe1464f0a96929e40a92a12921c
devlocalhost <lazarecobani@gmail.com>
2023-10-02 01:07:34 +0300

    dynamic route creation

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(" -- laurel_updates -- ")
+
+json_dir = 'roms/a13'
+
+def list_json_files(directory):
+    json_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            json_files.append(filename)
+    return json_files
+
+for json_file in list_json_files(json_dir):
+    rom_name = json_file.replace(".json", "")
+
+    def json_route(filename):
+        with open(os.path.join(json_dir, filename)) as file:
+            data = json.load(file)
+
+        return render_template(f"roms/{rom_name}.html", data=data)
+
+    app.add_url_rule(f'/roms/{rom_name}', json_file, json_route, defaults={'filename': json_file})
 
 @app.route("/")
 def main_route():
