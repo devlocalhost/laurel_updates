f5c1bde7a08206e38b83c8336b60197006690a86
devlocalhost <lazarecobani@gmail.com>
2023-10-02 00:20:43 +0300

    kernels and roms routes

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(" -- laurel_updates -- ")
 @app.route("/")
 def main_route():
     return render_template("index.html")
+
+@app.route("/roms")
+def roms():
+    return render_template("roms.html")
+
+@app.route("/kernels")
+def kernels():
+    return render_template("kernels.html")
