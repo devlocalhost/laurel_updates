ed4277e00929ec28fdc448f9a2b531e88a10fe76
devlocalhost <lazarecobani@gmail.com>
2023-10-07 12:31:35 +0300

    new rom

--- dump.py
+++ dump.py
@@ -13,12 +13,13 @@ data = {
     "status": sys.argv[6],
     "support": sys.argv[7],
     "rdp": sys.argv[8],
+    "notes": sys.argv[9],
     "downloads":
     {
         "editions":
         {
-            "vanilla": sys.argv[9] if sys.argv[9] != "null" else None,
-            "gapps": sys.argv[10]
+            "vanilla": sys.argv[10] if sys.argv[10] != "null" else None,
+            "gapps": sys.argv[11]
         }
     }
 }
@@ -28,10 +29,10 @@ json_data = json.dumps(data, indent=4)
 confirm = input(json_data + "\n\nOK? y/n\n -> ")
 
 if confirm == "y":
-    with open(f"roms/{sys.argv[11]}/{sys.argv[12]}.json", "w+") as json_file:
+    with open(f"roms/{sys.argv[12]}/{sys.argv[13]}.json", "w+") as json_file:
         json_file.write(json_data)
 
-    with open(f"roms/{sys.argv[11]}/{sys.argv[12]}.json") as json_file:
+    with open(f"roms/{sys.argv[12]}/{sys.argv[13]}.json") as json_file:
         sys.exit(json.loads(json_file.read()))
 
 else:
