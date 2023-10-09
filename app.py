df409613e47dabe13adada2450b857078e6bdc8b
devlocalhost <lazarecobani@gmail.com>
2023-10-09 17:39:53 +0300

    big again + caching

--- app.py
+++ app.py
@@ -8,10 +8,29 @@ import json
 import time
 import mistune
 from flask import Flask, render_template, make_response
+from cachelib.file import FileSystemCache
 
 app = Flask(" -- laurel_updates -- ")
+cache = FileSystemCache('.flask_cache')
+
 nl = "\n"
 android_versions = ["roms/13", "roms/12", "roms/11"]
+
+
+def generate_html(template_name, **context):
+    template_path = os.path.join(app.template_folder, template_name)
+    template_mtime = os.path.getmtime(template_path)
+
+    cache_key = f"{template_name}:{json.dumps(context, sort_keys=True)}"
+
+    cached_html = cache.get(cache_key)
+    if cached_html is not None and cached_html['mtime'] == template_mtime:
+        return cached_html['html']
+
+    html = render_template(template_name, **context)
+    cache.set(cache_key, {'html': html, 'mtime': template_mtime})
+
+    return html
 
 
 def list_json_files(directory):
@@ -28,7 +47,7 @@ def list_json_files(directory):
 def index():
     """index"""
 
-    return render_template("index.html")
+    return generate_html("index.html")
 
 
 @app.route("/help")
@@ -42,7 +61,7 @@ def help_route():
             with open(f"help/{file}", encoding="utf-8") as artc_file:
                 articles[artc_file.readline().replace("# ", "").replace(nl, "")] = file.removesuffix(".md")
 
-    return render_template("help.html", articles=articles)
+    return generate_html("help.html", articles=articles)
 
 @app.route("/help/<article_name>")
 def help_artc(article_name):
@@ -57,7 +76,7 @@ def help_artc(article_name):
         title = file.readline().removeprefix("# ").removesuffix(nl)
         data = mistune.html(file.read())
 
-    return render_template("help_template.html", data=data, title=title)
+    return generate_html("help_template.html", data=data, title=title)
 
 
 @app.route("/roms")
@@ -92,7 +111,7 @@ def roms():
 
             roms_data[version].append(data)
 
-    return make_response(render_template("roms.html", roms_data=roms_data))
+    return make_response(generate_html("roms.html", roms_data=roms_data))
 
 
 @app.route("/roms/<string:rom_name>_<int:version>")
@@ -105,9 +124,9 @@ def rom_route(rom_name, version):
         with open(json_path, "r", encoding="utf-8") as json_file:
             data = json.load(json_file)
 
-        return render_template("rom_template.html", data=data)
+        return generate_html("rom_template.html", data=data)
 
-    return render_template("404.html")
+    return generate_html("404.html")
 
 
 @app.route("/kernels")
@@ -122,7 +141,7 @@ def kernels():
             j_data = json.load(file)
             data["kernels"].append(j_data)
 
-    return make_response(render_template("kernels.html", data=data))
+    return make_response(generate_html("kernels.html", data=data))
 
 
 @app.route("/kernels/<kernel_name>")
@@ -132,18 +151,18 @@ def kernel_route(kernel_name):
     kernel_file = os.path.join("kernels", kernel_name + ".json")
 
     if not os.path.exists(kernel_file):
-        return render_template("404.html")
+        return generate_html("404.html")
 
     with open(kernel_file, encoding="utf-8") as file:
         data = json.load(file)
 
-    return render_template("kernel_template.html", data=data)
+    return generate_html("kernel_template.html", data=data)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html")
+    return generate_html("404.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
