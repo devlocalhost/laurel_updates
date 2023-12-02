3dbb8f95b23b038260220ed55a05988b78937ddf
devlocalhost <lazarecobani@gmail.com>
2023-12-02 03:00:01 +0200

    big

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+// fade in/out
+
 window.addEventListener("beforeunload", function () {
     document.body.classList.add("fade-out");
 });
@@ -7,10 +9,8 @@ document.querySelectorAll("a").forEach(function (link) {
         link.addEventListener("click", function (event) {
             event.preventDefault();
 
-            // Fade out the current page
             document.body.classList.add("fade-out");
 
-            // Once the fade out animation is complete, navigate to the new page
             document.body.addEventListener("animationend", function () {
                 window.location.href = link.href;
             });
@@ -18,9 +18,13 @@ document.querySelectorAll("a").forEach(function (link) {
     }
 });
 
+// fade in/out
+
+// hiding/showing navbar on scroll
+
 var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (window.innerWidth < 767) {
         var currentScrollPos = window.pageYOffset;
         var navbar = document.getElementsByClassName("navbar")[0];
@@ -33,5 +37,61 @@ window.onscroll = function() {
 
         prevScrollpos = currentScrollPos;
     }
+};
+
+// hiding/showing navbar on scroll
+
+// copy code on code tag click
+
+window.onload = function () {
+    const codeElements = document.querySelectorAll("code");
+
+    if (codeElements.length > 0) {
+        codeElements.forEach(function (codeElement) {
+            codeElement.onclick = function () {
+                document.execCommand("copy");
+            };
+
+            codeElement.addEventListener("copy", function (event) {
+                event.preventDefault();
+
+                if (event.clipboardData) {
+                    const textToCopy =
+                        codeElement.textContent || codeElement.innerText;
+                    event.clipboardData.setData("text/plain", textToCopy);
+                    console.log(event.clipboardData.getData("text"));
+                }
+            });
+        });
+    } else {
+        console.error("No code elements found.");
+    }
+};
+
+// copy code on code tag click
+
+// "debug" stylesheet
+
+function setCount(count) {
+    localStorage.setItem('clickCount', count);
 }
 
+function getCount() {
+    return parseInt(localStorage.getItem('clickCount')) || 0;
+}
+
+let isDebugStylesheetVisible = false;
+
+function toggleStylesheet() {
+    const clickCount = getCount();
+    setCount(clickCount + 1);
+
+    if (clickCount % 3 === 0) {
+        isDebugStylesheetVisible = !isDebugStylesheetVisible;
+
+        const debugStylesheet = document.getElementById('debug-stylesheet');
+        debugStylesheet.disabled = !isDebugStylesheetVisible;
+    }
+}
+
+// "debug" stylesheet
